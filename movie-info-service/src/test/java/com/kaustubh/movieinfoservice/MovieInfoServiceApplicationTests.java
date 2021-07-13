24d90e3f4bb9cb315d9241f88ff97c3772e71754
Kaustubh Kasadiwar <koistubh.kasadiwar@in.ncs-i.com>
2021-07-13 16:00:28 +0530

    initialze 3 projects

---
+++ movie-info-service/src/test/java/com/kaustubh/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaustubh.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
